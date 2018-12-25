463b3cea685f367a64aa6be8ff3c35ff9f383288
jessdatascience <jessdata@hotmail.com>
2018-12-25 14:33:18 -0500

    cachematrix 1st

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions "makeCacheMatrix" and "cacheSolve" are used to create
+## a special matrix and cache the inverse of the matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes on a matrix, builds four functions( set(),get(),
+## setinverse(), getinverse() ), and returns a list of functions to the parent 
+## environment.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inver <- NULL       ## initialize x and inver that store key information 
+        set <- function(y) {
+                x <<- y
+                inver <<- NULL}
+        get <- function() x
+        setinverse <- function(inverse) inver <<- inverse        
+        getinverse <- function() inver       
+        ## R retrieve inver value in the parent environment
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        ## creates a list of function and returns it
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve returns the inverse of the matrix created in makeCacheMatrix.
+## It first check whether the inver is NULL, if is TRUE, it means we have a 
+## cached inverse and will print "getting cached data" and return it to the
+## parent environment. If the condition is FALSE, R will solve the inverse and
+## set the inverse in the inver and then returns the value of the inver to the
+## parent environment.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inver<-x$getinverse()
+        
+        if(!is.null(inver)){
+                message("getting cached data")  ## if the condition is TRUE
+                return(inver)}
+        
+        data<- x$get()  
+        inver<-solve(data,...)
+        x$setinverse(inver)
+        inver
+        ## if the condition is FALSE
 }
